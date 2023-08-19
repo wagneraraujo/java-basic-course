66d1cb7650ebbda651dd0bfdcad61bbf9897fb6f
wagner <wagneraraujo15@gmail.com>
2023-08-19 02:50:59 -0300

    orgafnizando codigo

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,5 @@ import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
 
-
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        Product[] vect = new Product[n];
-
-
-        for(int i = 0; i < vect.length; i++){
-            sc.nextLine();
-
-            String name = sc.nextLine();
-            double price = sc.nextDouble();
-            vect[i] = new Product(name, price);
-        }
-
-        double sum = 0.0;
-        for(int i=0; i < vect.length; i++){
-            sum += vect[i].getPrice();
-        }
-        double avg = sum / vect.length;
-        System.out.printf("preco total = %.2f%n", avg );
-
-
-
-        sc.close();
-    }
 }
